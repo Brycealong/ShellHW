0430d582187da5205001939ba0ddb47df101e8e0
darcine <102016491+darcine@users.noreply.github.com>
2022-04-06 00:38:25 +0800

    Add files via upload

--- lab4/codon.c
+++ lab4/codon.c
@@ -1,9 +1,4 @@
-/* lab4/include/codon.h */
-#ifndef __CODON__
-#define __CODON__
-char condon(int base1, int base2, int base3);
-int transla(char DNA);
-#endif
+
 
 /* lab4/src/codon.c */
 #include <stdio.h>
@@ -24,27 +19,28 @@ char condon(int base1, int base2, int base3)
   base[A][G][A]='R';base[A][G][G]='R';
   base[A][G][C]='S';base[A][G][T]='S';
 
-  for(i=0;i<3;i++) base[A][C][i]='T';
+  for(i=0;i<=3;i++) base[A][C][i]='T';
 
-  base[A][T][A]='I';base[A][T][G]='M';
+  base[A][T][A]='I';base[A][T][T]='M';
+  base[A][T][C]='I';base[A][T][G]='M';
 
   base[G][A][A]='E';base[G][A][G]='E';
   base[G][A][C]='N';base[G][A][T]='N';
 
-  for(i=0;i<3;i++) base[G][G][i]='G';
+  for(i=0;i<=3;i++) base[G][G][i]='G';
 
-  for(i=0;i<3;i++) base[G][C][i]='A';
+  for(i=0;i<=3;i++) base[G][C][i]='A';
 
-  for(i=0;i<3;i++) base[G][T][i]='V';
+  for(i=0;i<=3;i++) base[G][T][i]='V';
 
   base[C][A][A]='Q';base[C][A][G]='Q';
   base[C][A][C]='H';base[C][A][T]='H';
 
-  for(i=0;i<3;i++) base[C][G][i]='R';
+  for(i=0;i<=3;i++) base[C][G][i]='R';
 
-  for(i=0;i<3;i++) base[C][C][i]='P';
+  for(i=0;i<=3;i++) base[C][C][i]='P';
 
-  for(i=0;i<3;i++) base[C][T][i]='L';
+  for(i=0;i<=3;i++) base[C][T][i]='L';
 
   base[T][A][A]='*';base[T][A][G]='*';
   base[T][A][C]='Y';base[T][A][T]='Y';
@@ -52,7 +48,7 @@ char condon(int base1, int base2, int base3)
   base[T][G][A]='*';base[T][G][G]='W';
   base[T][G][C]='C';base[T][G][T]='C';
 
-  for(i=0;i<3;i++) base[T][C][i]='S';
+  for(i=0;i<=3;i++) base[T][C][i]='S';
 
   base[T][T][A]='L';base[T][T][G]='L';
   base[T][T][C]='F';base[T][T][T]='F';
@@ -62,52 +58,81 @@ char condon(int base1, int base2, int base3)
 
 int transla(char DNA)
 {
-  if(DNA=='A') return 0;
-  else if(DNA=='T') return 3;
-  else if(DNA=='C') return 2;
-  else if(DNA=='G') return 1;
+  if(DNA=='A'||DNA=='a') return 0;
+  else if(DNA=='T'||DNA=='t') return 3;
+  else if(DNA=='C'||DNA=='c') return 2;
+  else if(DNA=='G'||DNA=='g') return 1;
   else return -1;
 }
 
-/* lab4/src/main.c */
-#include "codon.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 
 int main()
 {
-  int i;
-  char dna[3];
-  int base[3];
-  char amino;
+    int i;
+    char dna[3];
+    int base[3];
+    char amino;
 
-  fflush(stdin);
+    fflush(stdin);
 
-  printf("please input:");
+    printf("please input:");
 
-  dna[0]=getchar();
-  dna[1]=getchar();
-  dna[2]=getchar();
-
-  while(dna[0] == 'G' || dna[1] != 'T' || dna[2] != 'G' ){
-
-    dna[0]=dna[1];
-    dna[1]=dna[2];
-    dna[2]=getchar();
-
-  }
-
-  while(1){
-    for(i=0;i<=2;++i) base[i]=transla(dna[i]);
-
-    amino = condon(base[0],base[1],base[2]);
-    printf("%c",amino);
-    if (amino == '*') break;
     dna[0]=getchar();
     dna[1]=getchar();
     dna[2]=getchar();
-  }
-  printf("\n");
 
-  return 0;
+
+    for(i=0;i<=2;++i){
+            if (transla(dna[i])==-1) {
+                printf("\n");
+                printf("error\n");
+                exit (0);
+    }
+  }
+
+    while(1){
+            while((dna[0] == 'G' || dna[0] == 'g') || (dna[1] != 'T' && dna[1] != 't')|| (dna[2] != 'G' && dna[2] != 'g')){
+                dna[0]=dna[1];
+                dna[1]=dna[2];
+                dna[2]=getchar();
+                if (dna[2]=='\n') exit (0);
+                if (transla(dna[2])==-1) {
+                printf("\n");
+                printf("error\n");
+                exit (0);
+                }
+            }
+
+            while(1){
+                for(i=0;i<=2;++i) {
+                    if (transla(dna[i])!=-1) base[i]=transla(dna[i]);
+                    else {
+                        printf("\n");
+                        printf("error\n");
+                        exit (0);
+                    }
+                }
+
+            amino = condon(base[0],base[1],base[2]);
+            printf("%c",amino);
+            if (amino == '*') break;
+            dna[0]=getchar();
+            if(dna[0]=='\n') {printf(" It's the end of the sequence."); exit(0);}
+            dna[1]=getchar();
+            if(dna[1]=='\n') {printf(" It's the end of the sequence."); exit(0);}
+            dna[2]=getchar();
+            if(dna[2]=='\n') {printf(" It's the end of the sequence."); exit(0);}
+        }
+
+            dna[0]=getchar();
+            if(dna[0]=='\n') {printf("\n"); exit(0);}
+            dna[1]=getchar();
+            if(dna[1]=='\n') {printf("\n"); exit(0);}
+            dna[2]=getchar();
+            if(dna[2]=='\n') {printf("\n"); exit(0);}
+
+        printf("\n");
+    }
+    return 0;
 }
