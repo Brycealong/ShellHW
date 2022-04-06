ce873eb26ff83b76ae900f004cea0a91adc7fd41
JialuWei <98601389+JialuWei@users.noreply.github.com>
2022-04-06 22:46:01 +0800

    Add files via upload

---
+++ lab4/codon.h
@@ -0,0 +1,9 @@
+/* lab4/include/codon.h */
+#ifndef __CODON__
+#define __CODON__
+char codon(int base1, int base2, int base3);
+int transla(char DNA);
+int *translation(char DNA[]);
+char *trans_amino(char DNA[]);
+#endif
+
