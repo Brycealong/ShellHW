8c1eca58759885962cf848840de0ea2a0015a568
SylviaMei <103017464+SylviaMei@users.noreply.github.com>
2022-04-06 15:45:33 +0800

    Update condons.c
    Correct the mismatch in array's name and some other mistakes.

--- lab4/condons.c
+++ lab4/condons.c
@@ -6,7 +6,7 @@
 
 char condon(int base1, int base2, int base3)
 {
-    char amino[4][4][4];
+    char base[4][4][4];
     int i;
 
     base[A][A][A]='K';base[A][A][G]='K';
@@ -15,7 +15,7 @@ char condon(int base1, int base2, int base3)
     base[A][G][A]='R';base[A][G][G]='R';
     base[A][G][C]='S';base[A][G][T]='S';
 
-    for(i=0;i<3;i++) base[A][C][i]='T';
+    for(i=0;i<=3;i++) base[A][C][i]='T';
 
     base[A][T][A]='I';base[A][T][G]='M';
     base[A][T][C]='I';base[A][T][T]='I';
@@ -23,20 +23,20 @@ char condon(int base1, int base2, int base3)
     base[G][A][A]='E';base[G][A][G]='E';
     base[G][A][C]='N';base[G][A][T]='N';
 
-    for(i=0;i<3;i++) base[G][G][i]='G';
+    for(i=0;i<=3;i++) base[G][G][i]='G';
 
-    for(i=0;i<3;i++) base[G][C][i]='A';
+    for(i=0;i<=3;i++) base[G][C][i]='A';
 
-    for(i=0;i<3;i++) base[G][T][i]='V';
+    for(i=0;i<=3;i++) base[G][T][i]='V';
 
     base[C][A][A]='Q';base[C][A][G]='Q';
     base[C][A][C]='H';base[C][A][T]='H';
 
-    for(i=0;i<3;i++) base[C][G][i]='R';
+    for(i=0;i<=3;i++) base[C][G][i]='R';
 
-    for(i=0;i<3;i++) base[C][C][i]='P';
+    for(i=0;i<=3;i++) base[C][C][i]='P';
 
-    for(i=0;i<3;i++) base[C][T][i]='L';
+    for(i=0;i<=3;i++) base[C][T][i]='L';
 
     base[T][A][A]='*';base[T][A][G]='*';
     base[T][A][C]='Y';base[T][A][T]='Y';
@@ -44,10 +44,10 @@ char condon(int base1, int base2, int base3)
     base[T][G][A]='*';base[T][G][G]='W';
     base[T][G][C]='C';base[T][G][T]='C';
 
-    for(i=0;i<3;i++) base[T][C][i]='S';
+    for(i=0;i<=3;i++) base[T][C][i]='S';
 
     base[T][T][A]='L';base[T][T][G]='L';
     base[T][T][C]='F';base[T][T][T]='F';
 
-    return amino[base1][base2][base3];
+    return base[base1][base2][base3];
 }
