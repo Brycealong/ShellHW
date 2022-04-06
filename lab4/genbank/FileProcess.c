5fe65d86aa672c52e5c0415d6798d81ac51fe961
SylviaMei <103017464+SylviaMei@users.noreply.github.com>
2022-04-06 21:47:18 +0800

    Update FileProcess.c
    Add some new functions

--- lab4/genbank/FileProcess.c
+++ lab4/genbank/FileProcess.c
@@ -4,11 +4,11 @@
 #define MAX_LINE 210
 #define MAX_COLUMN 81
 
-int main(){
+int main(int argc, char *argv[]){
     FILE *fp;
     char file[MAX_LINE][MAX_COLUMN]={0};
     char info[MAX_LINE][20][15]={0};
-    int i = -1, j = 0;
+    int i = -1, j = 0, k;
     if((fp = fopen("./NM000207.gb","r"))==NULL){  //open file
         perror("Fail to open!\n");
         return -1;
@@ -23,5 +23,41 @@ int main(){
     }
     fclose(fp);  //close file
     //now the file is saved in two arrays
+    
+    if(argv[1] == 'L')printf("Locus: %s",info[0][1]);//print locus
+
+    if(argv[1] == 'l')printf("Length: %s %s",info[0][2],info[0][3]);// print length
+
+    if(argv[1] == 'D'){  //print definition
+        printf("Definition:");
+        for(i=1;i<=15;i++){
+            printf(" %s",info[1][i]);
+            if(info[1][i][0]==NULL)break;
+        }
+    }
+
+    if(argv[1] == 'S'){  //print DNA sequence
+        for(i=0;i<=MAX_LINE;i++){
+            if(strcmp("ORIGIN",info[i][0])==0){
+                printf("The DNA sequence is:\n");
+                for(j=i+1;j<=209;j++){
+                    for(k=1;k<=9;k++)printf("%s",info[j][k]);
+                }
+            }
+        }
+    }
+
+
+    if(argv[1] == 'F'){  //print fasta file
+        for(i=0;i<=MAX_LINE;i++){
+            if(strcmp("ORIGIN",info[i][0])==0){
+                printf(">NM_000207\n");
+                for(j=i+1;j<=209;j++){
+                    for(k=1;k<=9;k++)printf("%s",info[j][k]);
+                }
+            }
+        }
+    }
+    
     return 0;
 }
