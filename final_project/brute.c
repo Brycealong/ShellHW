fb84b55a514a7c9e380e93f85377be08e7add8a9
Brycealong <99854950+Brycealong@users.noreply.github.com>
2022-06-18 14:37:05 +0800

    Update brute.c

--- final_project/brute.c
+++ final_project/brute.c
@@ -1,1 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_NUM 400 //the maximum number of DNA sequences
+#define MAX_LEN 800 //the maximum length of DNA sequences
 
+//tell user the usage of this program
+void usage(){
+    printf( "USAGE:./apsp FILENAME\n" );
+    return;
+}
+
+//calculate the maximum overlap length between the prefix of s2 and the suffix of s1
+int overlen(char s2[], char s1[]){
+    int min = find_min(s1, s2);
+    int i, j;
+    int overlen = 0;
+    for (i = 1; i < min; i++){
+        int ct = 0;
+        for (j = 0; j < i; j++){
+            if (s1[strlen(s1)-i+j] == s2[j]) ct++;
+        }
+        if (ct == i) overlen = i;
+    }
+    return overlen;
+}
+
+//find the shortest between the length of s1 and the length of s2
+int find_min(char s1[], char s2[]){
+    if (strlen(s1) >= strlen(s2)) return strlen(s2);
+    else return strlen(s1);
+}
+
+int main(int argc, char** argv[]){
+    if(argc != 2){
+        printf("Wrong Input!\n");
+        usage();
+        return -1;
+    }
+
+    char* FILE_NAME=argv[1];
+    FILE *fp;
+    fp=fopen(FILE_NAME,"r");
+    if(fp==NULL)
+    {
+        perror("Fail to open!\n");
+        return -1;
+    }
+
+    char DNA[MAX_NUM][MAX_LEN]={'\0'};
+    char tmp;
+    int i=-1,j=0,flag=0;
+    while((tmp=fgetc(fp))!=EOF)
+    {
+        if(tmp=='>')
+        {
+            DNA[i][j]='\0';
+            i++;j=0;
+            flag=1;
+        }
+        if(flag==1)
+        {
+            if(tmp=='\n')
+            {
+                flag=0;
+            }
+        }
+        if(flag==0)
+        {
+            if((tmp=='A')|(tmp=='G')|(tmp=='C')|(tmp=='T'))
+            {
+                DNA[i][j]=tmp;
+                j++;
+            }
+        }
+
+    }
+    fclose(fp);
+
+    int num=i+1;
+    int arr[num][num];
+    for(i=0;i<num;i++){
+        for(j=0;j<num;j++){
+            arr[i][j]=overlen(DNA[i],DNA[j]);
+            printf("%d,",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
