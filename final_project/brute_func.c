8810f43eede126af2f5018938bc4ff3e0ec77daf
Brycealong <99854950+Brycealong@users.noreply.github.com>
2022-06-18 15:08:04 +0800

    Add files via upload

---
+++ final_project/brute_func.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+int find_min(char s1[], char s2[]){
+  if (strlen(s1) >= strlen(s2)) return strlen(s2);
+  else return strlen(s1);
+}
+
+int overlen(char s2[], char s1[]){
+    int min = find_min(s1, s2);
+    int i, j;
+    int overlen = 0;
+    for (i = 1; i < min; i++){
+        int ct = 0;
+        for (j = 0; j < i; j++){
+            if (s1[strlen(s1)-i+j] == s2[j]) ct++;
+        }
+        if (ct == i) overlen = i;
+    }
+    return overlen;
+}
