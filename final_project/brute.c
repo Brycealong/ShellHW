ec7b1df74d6c5e21ce34e0d6e3878e784c2d929a
Brycealong <99854950+Brycealong@users.noreply.github.com>
2022-06-18 14:49:35 +0800

    Update brute.c

--- final_project/brute.c
+++ final_project/brute.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "brute.h"
 #define MAX_NUM 400 //the maximum number of DNA sequences
 #define MAX_LEN 800 //the maximum length of DNA sequences
 
@@ -11,25 +12,10 @@ void usage(){
 }
 
 //calculate the maximum overlap length between the prefix of s2 and the suffix of s1
-int overlen(char s2[], char s1[]){
-    int min = find_min(s1, s2);
-    int i, j;
-    int overlen = 0;
-    for (i = 1; i < min; i++){
-        int ct = 0;
-        for (j = 0; j < i; j++){
-            if (s1[strlen(s1)-i+j] == s2[j]) ct++;
-        }
-        if (ct == i) overlen = i;
-    }
-    return overlen;
-}
+int overlen(char s2[], char s1[]);
 
 //find the shortest between the length of s1 and the length of s2
-int find_min(char s1[], char s2[]){
-    if (strlen(s1) >= strlen(s2)) return strlen(s2);
-    else return strlen(s1);
-}
+int find_min(char s1[], char s2[]);
 
 int main(int argc, char** argv[]){
     if(argc != 2){
