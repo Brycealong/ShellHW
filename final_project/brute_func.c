35ad33424c8f7c0b0e5b980ddfd0c3bb9343733c
Brycealong <99854950+Brycealong@users.noreply.github.com>
2022-06-19 16:35:04 +0800

    Update brute_func.c

--- final_project/brute_func.c
+++ final_project/brute_func.c
@@ -9,7 +9,7 @@ int overlen(char s2[], char s1[]){
     int min = find_min(s1, s2);
     int i, j;
     int overlen = 0;
-    for (i = 1; i < min; i++){
+    for (i = 1; i <= min; i++){
         int ct = 0;
         for (j = 0; j < i; j++){
             if (s1[strlen(s1)-i+j] == s2[j]) ct++;
